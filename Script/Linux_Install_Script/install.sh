6824d7b29ea77eaf90299209def8aab09fe29c7a
will-hut <22407192+will-hut@users.noreply.github.com>
2024-01-29 02:52:28 -0600

    create first working iteration of install script

--- Script/Linux_Install_Script/install.sh
+++ Script/Linux_Install_Script/install.sh
@@ -2,40 +2,35 @@
 
 repo_name="hub75-fpga"
 
-#Check for sudo access
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
+
+echo [Beginning setup]
+echo Attempting to get sudo...
+if [[ ! $(sudo echo 0) ]]; then exit; fi
 
 
-echo Beginning setup...
-sleep 3
+echo [Making sure packages are up to date]
+sudo apt update
+sudo upgrade -y
 
 
-echo Making sure packages are up to date:
-apt update
-apt upgrade -y
+echo [Installing build tools]
+sudo install build-essential -y
 
 
-echo Installing build tools:
-apt install build-essential -y
-
-
-echo Grabbing latest repository:
+echo [Grabbing latest repository]
 git clone https://github.com/will-hut/$repo_name
 cd $repo_name/SW/SenderService
 
-echo Building FTDI sender program:
+echo [Building FTDI sender program]
 make
 
-echo Adding systemd service:
-cp sender /usr/local/bin/
-cp screen.service /lib/systemd/system/
-systemctl enable screen.service
-systemctl start screen.service
+echo [Adding systemd service]
+sudo cp sender /usr/local/bin/
+sudo cp screen.service /etc/systemd/system/
+sudo systemctl enable screen.service
+sudo systemctl start screen.service
 
-echo Blacklisting ftdi_sio module
-cp blacklist-ftdi.conf /etc/modprobe.d/
+echo [Blacklisting ftdi_sio module]
+sudo cp blacklist-ftdi.conf /etc/modprobe.d/
 
 echo Done! Please reboot.
